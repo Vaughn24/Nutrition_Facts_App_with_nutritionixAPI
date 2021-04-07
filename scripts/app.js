90b48dc474abf34de8b61f075331e501b81cedaf
Vaughn2400 <kennethgolosinda24@gmail.com>
2021-04-07 18:28:16 +0800

    Changes1

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,28 @@
-fetch(
-    "https://trackapi.nutritionix.com/v2/search/instant?query=apple",// I-backtick nlng siguro yung sa query na part para pag search
-    {
-    headers: {
-    "x-app-id": "f4b3ec1a",
-    "x-app-key": "63ee9abe0afb08659336e8666e5ffb86",
-    },
-    })
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-    })
 
+$('#test1').nutritionLabel();
 
+var search_food = ""
 
-    $('#test1').nutritionLabel();
+const input_field = document.querySelector("#search")
+
+input_field.addEventListener("keydown", (event) => {
+    if(event.keyCode == 13){
+
+        fetch(
+            `https://trackapi.nutritionix.com/v2/search/instant?query=${search_food}`,
+            {
+            headers: {
+            "x-app-id": "f4b3ec1a",
+            "x-app-key": "63ee9abe0afb08659336e8666e5ffb86",
+            },
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+    }
+    else {
+        search_food = input_field.value 
+    }
+
+})

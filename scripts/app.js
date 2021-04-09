83fae3dd1ea3825bad1b1cbe56f44798e78904c6
Vaughn2400 <kennethgolosinda24@gmail.com>
2021-04-09 15:10:48 +0800

    Changes1

--- scripts/app.js
+++ scripts/app.js
@@ -14,6 +14,38 @@ function getfoodnutrients(search_food){
         .then(res => res.json())
         .then(data => {
             console.log(data)
+
+
+
+            const calories = document.querySelector("#calories")
+            const calories_from_fat = document.querySelector("calories-from-fat")
+            const total_fat = document.querySelector("#total-fat")
+            const saturated_fat = document.querySelector("saturated-fat")
+            const polyunsaturated_fat = document.querySelector("#polyunsaturated_fat")
+            const monounsaturated_fat = document.querySelector("#monosaturated-fat")
+            const cholesterol = document.querySelector("#cholesterol")
+            const sodium = document.querySelector("#sodium")
+            const sodium_p = document.querySelector("#sodium-p")
+            const total_carbohydrates = document.querySelector("#carbohydrates")
+            const total_carbohydrates_p = document.querySelector("#carbohydrates-p")
+            const dietary_fiber = document.querySelector("#dietary-fiber")
+            const dietary_fiber_p = document.querySelector("#dietary-fiber-p")
+            const sugars = document.querySelector("#sugars")
+            const protein = document.querySelector("#protein")
+            const vitamin_a = document.querySelector("#vitamin-a")
+            const vitamin_b = document.querySelector("#vitamin-b")
+            const vitamin_c = document.querySelector("#vitamin-c")
+            const vitamin_d = document.querySelector("#vitamin-d")
+            const calcium = document.querySelector("#calcium")
+            const iron = document.querySelector("#iron")
+            const potassium = document.querySelector("#potassium")
+
+
+            calories.innerHTML = data.foods[0].nf_calories;
+            total_fat.innerHTML = data.foods[0].nf_total_fat;
+            saturated_fat
+
+
         })
 
 
@@ -32,6 +64,7 @@ function getfood(search_food){
         .then(res => res.json())
         .then(data => {
             console.log(data)
+
         })
 
 }
@@ -48,3 +81,6 @@ input_field.addEventListener("keydown", (event) => {
     }
 
 })
+
+
+
